14527885294b01a1e4f21bb85f68010b5855d39b
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-10-28 23:30:44 +0000

    updated allowed host

--- FryedSauces/settings.py
+++ FryedSauces/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['167.172.50.142']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
