8e65fc911e1b9f8f146572bff681c50b802a5a48
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-10-28 22:16:28 +0000

    updated secret key

--- FryedSauces/settings.py
+++ FryedSauces/settings.py
@@ -10,7 +10,7 @@ from decouple import config
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = False
 
