a9667718a33d0088c7f42197038acf858b2edc2f
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-10-28 21:43:57 +0000

    Changed database name lowercase

--- FryedSauces/settings.py
+++ FryedSauces/settings.py
@@ -93,7 +93,7 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'FyedProject',
+            'NAME': 'fyedproject',
             'USER': 'delroybrown',
             'PASSWORD': 'william17',
             'HOST': 'localhost',
